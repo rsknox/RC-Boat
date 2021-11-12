558e6e2e45522c016aa96da221d972a9ea061f78
R.S <rsknox@gmail.com>
2021-11-12 05:21:24 -0600

    another try at reset to previous version

--- BoatCode/sketch_nov11a/sketch_nov11a.ino
+++ BoatCode/sketch_nov11a/sketch_nov11a.ino
@@ -4,6 +4,7 @@
 // this is a test from Dell XPS13
 // this is a test of first mod to dev branch
 // this is a test of committing and then reverting to a previous version
+// I think I have to do one more commit to dev then do reset to go back to current-1
 void setup() {
   // put your setup code here, to run once:
 
