c436b5bc1f7b856b97bdc275c35a5bf4a4f1f225
R.S <rsknox@gmail.com>
2021-11-12 04:43:41 -0600

    trying to clean up nov11a test sketch

--- BoatCode/sketch_nov11a/sketch_nov11a.ino
+++ BoatCode/sketch_nov11a/sketch_nov11a.ino
@@ -1,66 +1,9 @@
 
 // this is a test of github
- master
-￼
-rsknox committed 19 hours ago 
-1 parent c040a98 commit a8f14419d8966ca4dbeffca51c43ddf949a26b73
-￼Split￼Unified
-
- master
-￼
-rsknox committed 19 hours ago 
-1 parent c040a98 commit a8f14419d8966ca4dbeffca51c43ddf949a26b73
-￼Split￼Unified
 
 // this is a test from Dell XPS13
- master
-￼
-rsknox committed 19 hours ago 
-1 parent c040a98 commit a8f14419d8966ca4dbeffca51c43ddf949a26b73
-￼Split￼Unified
-
- master
-￼
-rsknox committed 19 hours ago 
-1 parent c040a98 commit a8f14419d8966ca4dbeffca51c43ddf949a26b73
-￼Split￼Unified
-
- master
-￼
-rsknox committed 19 hours ago 
-1 parent c040a98 commit a8f14419d8966ca4dbeffca51c43ddf949a26b73
-￼Split￼Unified
-
- master
-￼
-rsknox committed 19 hours ago 
-1 parent c040a98 commit a8f14419d8966ca4dbeffca51c43ddf949a26b73
-￼Split￼Unified
-
- master
-￼
-rsknox committed 19 hours ago 
-1 parent c040a98 commit a8f14419d8966ca4dbeffca51c43ddf949a26b73
-￼Split￼Unified
-
- master
-￼
-rsknox committed 19 hours ago 
-1 parent c040a98 commit a8f14419d8966ca4dbeffca51c43ddf949a26b73
-￼Split￼Unified
-
- master
-￼
-rsknox committed 19 hours ago 
-1 parent c040a98 commit a8f14419d8966ca4dbeffca51c43ddf949a26b73
-￼Split￼Unified
-
- master
-￼
-rsknox committed 19 hours ago 
-1 parent c040a98 commit a8f14419d8966ca4dbeffca51c43ddf949a26b73
-￼Split￼Unified
 // this is a test of first mod to dev branch
+
 void setup() {
   // put your setup code here, to run once:
 
