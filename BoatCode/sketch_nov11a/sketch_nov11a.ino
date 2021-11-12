0298530dc9624c6bc33fe42876354f473bc34674
R.S <rsknox@gmail.com>
2021-11-12 04:57:30 -0600

    added a comment line to test reverting to previous version

--- BoatCode/sketch_nov11a/sketch_nov11a.ino
+++ BoatCode/sketch_nov11a/sketch_nov11a.ino
@@ -3,7 +3,7 @@
 
 // this is a test from Dell XPS13
 // this is a test of first mod to dev branch
-
+// this is a test of committing and then reverting to a previous version
 void setup() {
   // put your setup code here, to run once:
 
